2aadde17f184e2c74e6c99ffc46ab6b5f604ae50
dinglenutsxnex-crypto <dinglenutsxnex@gmail.com>
2026-07-25 09:51:22 +0530

    Add h2d.Scene rendering with background color

--- src/Main.hx
+++ src/Main.hx
@@ -1,20 +1,29 @@
 import scenes.EnterPointScene;
+import h2d.Scene;
+import h2d.Graphics;
 
 class Main extends hxd.App {
 
 	static var enterPoint:EnterPointScene;
+	static var scene:Scene;
 
 	static function main() {
 		new Main();
 	}
 
 	override function init() {
-		// Initialize the entry point scene
+		scene = new Scene();
+
+		var g = new Graphics(scene);
+		g.beginFill(0x202040);
+		g.drawRect(0, 0, 800, 600);
+		g.endFill();
+
 		enterPoint = new EnterPointScene();
 		enterPoint.init();
 	}
 
 	override function update(dt:Float) {
-		// Game update loop
+		scene.render(engine);
 	}
 }
