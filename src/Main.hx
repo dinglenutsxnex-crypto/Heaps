6fdf594eee085b7543f81b2ee8afe589a25a601d
dinglenutsxnex-crypto <dinglenutsxnex@gmail.com>
2026-07-25 10:09:18 +0530

    Use s2d for rendering with background color and text

--- src/Main.hx
+++ src/Main.hx
@@ -1,29 +1,31 @@
 import scenes.EnterPointScene;
-import h2d.Scene;
 import h2d.Graphics;
 
 class Main extends hxd.App {
 
 	static var enterPoint:EnterPointScene;
-	static var scene:Scene;
 
 	static function main() {
 		new Main();
 	}
 
 	override function init() {
-		scene = new Scene();
+		engine.backgroundColor = 0xFF202040;
 
-		var g = new Graphics(scene);
-		g.beginFill(0x202040);
-		g.drawRect(0, 0, 800, 600);
+		var g = new Graphics(s2d);
+		g.beginFill(0x404080);
+		g.drawRect(0, 0, 200, 200);
 		g.endFill();
+
+		var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+		tf.text = "SF3";
+		tf.x = 100;
+		tf.y = 100;
 
 		enterPoint = new EnterPointScene();
 		enterPoint.init();
 	}
 
 	override function update(dt:Float) {
-		scene.render(engine);
 	}
 }
